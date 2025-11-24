fd9c51fbb8f5e8db39f458fe034e0265e03270b5
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-24 14:41:20 +0000

    Multi-line comments in Java with /*

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main{
     public static void main(String[] args){
         // My first JAVA Program
+        /*
+        This
+        is
+        a
+        multi
+        line
+        comment
+         */
         System.out.println("My name is Mortoti");
         System.out.print("My dream is to become the Mosh Hamedani of West Africa");
     }
