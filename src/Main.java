08e0e8e4aa1b3519eae86bac4b9688c705a73c73
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-24 14:39:12 +0000

    Comment writting in Java with //

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
+        // My first JAVA Program
         System.out.println("My name is Mortoti");
         System.out.print("My dream is to become the Mosh Hamedani of West Africa");
     }
