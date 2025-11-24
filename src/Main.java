76ab2ff662a7c28e01f9fec7aa63e7f75efe1e0a
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-24 15:14:02 +0000

    Working with variables and Data types

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,26 @@ public class Main{
         comment
          */
         System.out.println("My name is Mortoti");
-        System.out.print("My dream is to become the Mosh Hamedani of West Africa");
+        System.out.println("My dream is to become the Mosh Hamedani of West Africa");
+       // Creating Variables
+
+        // Integer Variables
+        int age = 19;
+        System.out.println("age: " + age);
+
+        // Double or Decimal Variables
+        double net_worth = 109.3;
+        System.out.println("My net worth is: " + net_worth + " Ghana Cedis");
+
+        // Character Variables
+        char grade = 'A';
+        System.out.println("I had grade " + grade + " in the exams");
+
+        // Boolean Variables
+        boolean isStudent = true;
+
+        System.out.println("It is " + isStudent + " that he is a student");
+
     }
 
 
